6a26986eae04d9df09a250a0831d9022a71f321f
DinuGabrielaLoredana <gdinu93@yahoo.com>
2018-05-06 14:34:45 +0300

    Create DigitalOutputs.h

---
+++ session1/DigitalOutputs.h
@@ -0,0 +1,30 @@
+/*
+ * DigitalOutputs.h
+ *
+ *  Created on: Feb 26, 2014
+ *      Author: catalin
+ */
+
+#ifndef DIGITALOUTPUTS_H_
+#define DIGITALOUTPUTS_H_
+
+
+typedef unsigned char byte;
+typedef unsigned int  uint8;
+
+#define ON          1
+#define OFF         0
+
+
+#define	LED1	0
+#define	LED2	1
+#define	LED3	2
+#define	LED4	3
+#define	LED5	4
+#define	LED6	5
+#define	LED7	6
+#define	LED8	7
+
+
+
+#endif /* DIGITALOUTPUTS_H_ */
